88bb3d1cbf33e963d32a378783e29f958c6ccdc9
Ian Sullivan <iansullivan88@gmail.com>
2014-04-13 16:47:26 +0100

    Commented some methods

--- bramble.js
+++ bramble.js
@@ -4,6 +4,18 @@ var fs = require('fs'),
     processRequestLoop = require('./lib/processing/process-request-loop'),
     processRequest = require('./lib/processing/process-request');
 
+/**
+* The method is the entry point to Bramble MVC. Calling it will export your site to the specified
+* output directory.
+*
+* @method buildSite
+* @param outputDirectory {String} That root directory to which files are exported
+* @param options {Object} An object to specify options when building the site
+* @param options.routes {[Object]} List of routes
+* @param options.controllers {Object} A name value collection of controller constructor functions
+* @param [options.urls=['/']] {String[]} The initial list of urls to process
+* @param [options.viewRenderer] {Function} The view renderer used to process views (Nunjucks is used by default)
+*/
 exports.buildSite = function(outputDirectory, options, callback) {
     
     var viewPath = options.viewPath || path.join(process.cwd(), 'lib', 'view');
