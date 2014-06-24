c480c2d231df237b45d321a7705981210d8b5ced
Ian Sullivan <iansullivan88@gmail.com>
2014-06-24 20:45:50 +0100

    Posts in the example site are now read from files rather than being defined in a single json file.

--- examples/catblog/lib/data/blog-repository.js
+++ examples/catblog/lib/data/blog-repository.js
@@ -1,133 +1,23 @@
-var blogPosts = [{
-    id:"introduction",
-    title:"Introduction to Cats",
-    date:new Date(2014, 5, 1),
-    content:"The domestic cat (Felis catus or Felis silvestris catus) is a small, usually furry, domesticated, and carnivorous mammal. It is often called the housecat when kept as an indoor pet, or simply the cat when there is no need to distinguish it from other felids and felines. Cats are often valued by humans for companionship and their ability to hunt vermin and household pests.",
-},
-{
-    id:"anatomy",
-    title:"Anatomy of Cats",
-    date:new Date(2014, 4, 1),
-    content:"Cats are similar in anatomy to the other felids, with strong, flexible bodies, quick reflexes, sharp retractable claws, and teeth adapted to killing small prey. Cat senses fit a crepuscular and predatory ecological niche. Cats can hear sounds too faint or too high in frequency for human ears, such as those made by mice and other small animals. They can see in near darkness. Like most other mammals, cats have poorer color vision and a better sense of smell than humans.",
-},
-{
-    id:"nature",
-    title:"Something About Cat Nature",
-    date:new Date(2014, 3, 1),
-    content:"Despite being solitary hunters, cats are a social species, and cat communication includes the use of a variety of vocalizations (mewing, purring, trilling, hissing, growling and grunting) as well as cat pheromones, and types of cat-specific body language.",
-},
-{
-    id:"another_post",
-    title:"Another Blog Post Title",
-    date:new Date(2014, 2, 1),
-    content:"Cats have a rapid breeding rate. Under controlled breeding, they can be bred and shown as registered pedigree pets, a hobby known as cat fancy. Failure to control the breeding of pet cats by neutering, and the abandonment of former household pets, has resulted in large numbers of feral cats worldwide, requiring population control.",
-},
-{
-    id:"another_post_2",
-    title:"Another Blog Post Title 2",
-    date:new Date(2014, 1, 1),
-    content:"Since cats were cult animals in ancient Egypt, they were commonly believed to have been domesticated there, but there may have been instances of domestication as early as the Neolithic from around 9500 years ago (7500 BC).",
-},
-{
-    id:"another_post_3",
-    title:"Another Blog Post Title 3",
-    date:new Date(2014, 0, 1),
-    content:"A genetic study in 2007 concluded that domestic cats are descended from African wildcats (Felis silvestris lybica) c. 8000 BC, in the Near East. Cats are the most popular pet in the world, and are now found in almost every place where humans live.",
-},
-{
-    id:"another_post_4",
-    title:"Another Blog Post Title 4",
-    date:new Date(2013, 11, 1),
-    content:"The English word cat (Old English catt) is in origin a loanword, introduced to many languages of Europe from Latin cattus and Byzantine Greek ??tta, including Portuguese and Spanish gato, French chat, German Katze, Lithuanian kate and Old Church Slavonic kotka, among others. The ultimate source of the word is Afroasiatic, presumably from Late Egyptian caute, the feminine of caus \"wildcat\". The word was introduced, together with the domestic animal itself, to the Roman Republic by the 1st century BC.[citation needed] An alternative word with cognates in many languages is English puss (pussycat). Attested only from the 16th century, it may have been introduced from Dutch poes or from Low German puuskatte, related to Swedish kattepus, or Norwegian pus, pusekatt. Similar forms exist in Lithuanian puiže and Irish puisín. The etymology of this word is unknown, but it may have simply arisen from a sound used to attract a cat.",
-},
-{
-    id:"another_post_5",
-    title:"Another Blog Post Title 5",
-    date:new Date(2013, 10, 1),
-    content:"A group of cats is referred to as a \"clowder\" or a \"glaring\", a male cat is called a \"tom\" or \"tomcat\" (or a \"gib\", if neutered), an unaltered female is called a \"queen\", and a pre-pubescent juvenile is referred to as a \"kitten\". Although spayed females have no commonly used name, in some rare instances immature or spayed females are referred to as a \"molly\".[citation needed] The male progenitor of a cat, especially a pedigreed cat, is its \"sire\", and its female progenitor is its \"dam\". In Early Modern English, the word kitten was interchangeable with the now-obsolete word catling.",
-},
-{
-    id:"another_post_6",
-    title:"Another Blog Post Title 6",
-    date:new Date(2013, 9, 1),
-    content:"A pedigreed cat is one whose ancestry is recorded by a cat fancier organization. A purebred cat is one whose ancestry contains only individuals of the same breed. Many pedigreed and especially purebred cats are exhibited as show cats. Cats of unrecorded, mixed ancestry are referred to as domestic short-haired or domestic long-haired cats, by coat type, or commonly as random-bred, moggies (chiefly British), or (using terms borrowed from dog breeding) mongrels or mutt-cats.",
-},
-{
-    id:"another_post_7",
-    title:"Another Blog Post Title 7",
-    date:new Date(2013, 8, 1),
-    content:"While the African wildcat is the ancestral subspecies from which domestic cats are descended, and wildcats and domestic cats can completely interbreed, there are several intermediate stages between domestic pet and pedigree cats on the one hand and those entirely wild animals on the other. The semi-feral cat is a mostly outdoor cat that is not owned by any one individual, but is generally friendly to people and may be fed by several households. Feral cats are associated with human habitation areas and may be fed by people or forage in rubbish, but are typically wary of human interaction.",
-},
-{
-    id:"another_post_8",
-    title:"Another Blog Post Title 8",
-    date:new Date(2013, 7, 1),
-    content:"The felids are a rapidly evolving family of mammals that share a common ancestor only 10–15 million years ago, and include, in addition to the domestic cat, lions, tigers, cougars, and many others. Within this family, domestic cats (Felis catus) are part of the genus Felis, which is a group of small cats containing approximately seven species (depending upon classification scheme). Members of the genus are found worldwide and include the jungle cat (Felis chaus) of southeast Asia, European wildcat (F. silvestris silvestris), African wildcat (F. s. lybica), the Chinese mountain cat (F. bieti), and the Arabian sand cat (F. margarita), among others.",
-},
-{
-    id:"another_post_9",
-    title:"Another Blog Post Title 9",
-    date:new Date(2013, 6, 1),
-    content:"All the cats in this genus share a common ancestor that probably lived around 6–7 million years ago in Asia. The exact relationships within the Felidae are close but still uncertain, e.g. the Chinese mountain cat is sometimes classified (under the name Felis silvestris bieti) as a subspecies of the wildcat, like the North African variety F. s. lybica. As domestic cats are little altered from wildcats, they can readily interbreed. This hybridization poses a danger to the genetic distinctiveness of wildcat populations, particularly in Scotland and Hungary, and possibly also the Iberian Peninsula.",
-},
-{
-    id:"another_post_10",
-    title:"Another Blog Post Title 10",
-    date:new Date(2013, 5, 1),
-    content:"The domestic cat was first classified as Felis catus by Carolus Linnaeus in the tenth edition of his Systema Naturae in 1758. However, because of modern phylogenetics, domestic cats are now usually regarded as another subspecies of the wildcat, Felis silvestris. This has resulted in mixed usage of the terms, as the domestic cat can be called by its subspecies name, Felis silvestris catus. Wildcats have also been referred to as various subspecies of F. catus, but in 2003 the International Commission on Zoological Nomenclature fixed the name for wildcats as F. silvestris. The most common name in use for the domestic cat remains F. catus, following a convention for domesticated animals of using the earliest (the senior) synonym proposed. Sometimes the domestic cat has been called Felis domesticus or Felis domestica, as proposed by German naturalist J. C. P. Erxleben in 1777, but these are not valid taxonomic names and have been used only rarely in scientific literature, because Linnaeus's binomial takes precedence.",
-},
-{
-    id:"another_post_11",
-    title:"Another Blog Post Title 11",
-    date:new Date(2013, 4, 1),
-    content:"Cats have either a mutualistic or commensal relationship with humans. However, in comparison to dogs, cats have not undergone major changes during the domestication process, as the form and behavior of the domestic cat are not radically different from those of wildcats, and domestic cats are perfectly capable of surviving in the wild. This limited evolution during domestication means that domestic cats tend to interbreed freely with wild relatives, distinguishing them from other domesticated animals.[citation needed] Fully domesticated house cats also often interbreed with feral F. catus populations. However, several natural behaviors and characteristics of wildcats may have pre-adapted them for domestication as pets. These traits include their small size, social nature, obvious body language, love of play, and relatively high intelligence;:12–17 they may also have an inborn tendency towards tameness.",
-},
-{
-    id:"another_post_12",
-    title:"Another Blog Post Title 12",
-    date:new Date(2013, 3, 1),
-    content:"There are two main theories about how cats were domesticated. In one, people deliberately tamed cats in a process of artificial selection, as they were useful predators of vermin. However, this has been criticized as implausible, because there may have been little reward for such an effort: cats generally do not carry out commands and, although they do eat rodents, other species such as ferrets or terriers may be better at controlling these pests. The alternative idea is that cats were simply tolerated by people and gradually diverged from their wild relatives through natural selection, as they adapted to hunting the vermin found around humans in towns and villages.",
-},
-{
-    id:"another_post_13",
-    title:"Another Blog Post Title 13",
-    date:new Date(2013, 2, 1),
-    content:"There is a population of Transcaucasian Black feral cats once classified as Felis daemon (Satunin, 1904), but now this population is considered to be a part of domestic cat.",
-},
-{
-    id:"another_post_14",
-    title:"Another Blog Post Title 14",
-    date:new Date(2013, 1, 1),
-    content:"The domesticated cat and its closest wild ancestor are both diploid organisms that possess 38 chromosomes and roughly 20,000 genes. About 250 heritable genetic disorders have been identified in cats, many similar to human inborn errors. The high level of similarity among the metabolisms of mammals allows many of these feline diseases to be diagnosed using genetic tests that were originally developed for use in humans, as well as the use of cats as animal models in the study of the human diseases.",
-},
-{
-    id:"another_post_15",
-    title:"Another Blog Post Title 15",
-    date:new Date(2013, 0, 1),
-    content:"Domestic cats are similar in size to the other members of the genus Felis, typically weighing between 4–5 kg (8.8–11.0 lb). However, some breeds, such as the Maine Coon, can occasionally exceed 11 kg (25 lb). Conversely, very small cats (less than 1.8 kg (4.0 lb)) have been reported. The world record for the largest cat is 21.3 kg (47 lb). The smallest adult cat ever officially recorded weighed around 1.36 kg (3.0 lb). Feral cats tend to be lighter as they have more limited access to food than house cats. In the Boston area, the average feral adult male will scale 3.9 kg (8.6 lb) and average feral female 3.3 kg (7.3 lb). Cats average about 23–25 cm (9–10 in) in height and 46 cm (18.1 in) in head/body length (males being larger than females), with tails averaging 30 cm (11.8 in) in length.",
-},
-{
-    id:"another_post_16",
-    title:"Another Blog Post Title 16",
-    date:new Date(2012, 11, 1),
-    content:"Cats have seven cervical vertebrae as do almost all mammals; 13 thoracic vertebrae (humans have 12); seven lumbar vertebrae (humans have five); three sacral vertebrae like most mammals (humans have five because of their bipedal posture); and a variable number of caudal vertebrae in the tail (humans retain three to five caudal vertebrae, fused into an internal coccyx).:11 The extra lumbar and thoracic vertebrae account for the cat's spinal mobility and flexibility. Attached to the spine are 13 ribs, the shoulder, and the pelvis. :16 Unlike human arms, cat forelimbs are attached to the shoulder by free-floating clavicle bones which allow them to pass their body through any space into which they can fit their heads.",
-},
-{
-    id:"another_post_17",
-    title:"Another Blog Post Title 17",
-    date:new Date(2012, 10, 1),
-    content:"The cat skull is unusual among mammals in having very large eye sockets and a powerful and specialized jaw.:35 Within the jaw, cats have teeth adapted for killing prey and tearing meat. When it overpowers its prey, a cat delivers a lethal neck bite with its two long canine teeth, inserting them between two of the prey's vertebrae and severing its spinal cord, causing irreversible paralysis and death. Compared to other felines, domestic cats have narrowly spaced canine teeth, which is an adaptation to their preferred prey of small rodents, which have small vertebrae. The premolar and first molar together compose the carnassial pair on each side of the mouth, which efficiently shears meat into small pieces, like a pair of scissors. These are vital in feeding, since cats' small molars cannot chew food effectively.:37",
-},
-{
-    id:"another_post_18",
-    title:"Another Blog Post Title 18",
-    date:new Date(2012, 9, 1),
-    content:"Cats, like dogs, are digitigrades. They walk directly on their toes, with the bones of their feet making up the lower part of the visible leg. Cats are capable of walking very precisely, because like all felines they directly register; that is, they place each hind paw (almost) directly in the print of the corresponding forepaw, minimizing noise and visible tracks. This also provides sure footing for their hind paws when they navigate rough terrain. Unlike most mammals, when cats walk, they use a \"pacing\" gait; that is, they move the two legs on one side of the body before the legs on the other side. This trait is shared with camels and giraffes. As a walk speeds up into a trot, a cat's gait will change to be a \"diagonal\" gait, similar to that of most other mammals (and many other land animals, such as lizards): the diagonally opposite hind and forelegs will move simultaneously.",
-}];
+// blog-repository.js reads blog posts from files in the /posts folder
 
-blogPosts.forEach(function(p) {
-    p.dateString = (p.date.getMonth() + 1) + "-" + p.date.getDate() + "-" + p.date.getFullYear();
-});
+var fs = require('fs'),
+	frontMatter = require('yaml-front-matter'),
+	path = require('path'),
+	postPath = path.join(path.dirname(require.main.filename), "posts"),
+	blogPosts = fs.readdirSync(postPath).map(function(filePath) {
+		var parsedContent = frontMatter.loadFront(fs.readFileSync(path.join(postPath, filePath))),
+			date = new Date(Date.parse(parsedContent.date));
+		
+		return {
+			id:path.basename(filePath).split('.')[0],
+			title:parsedContent.title,
+			date:date,
+			dateString:(date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear(),
+			content:parsedContent.__content
+		};
+	}).sort(function(a, b) {
+		return b.date.getTime() - a.date.getTime();
+	});
 
 exports.getPosts = function(callback) {
 	callback(null, blogPosts);
